0c1fd376f82b346b0bcc7d8cbdb162af7dbb0d07
benoit <benoitvicaireformation@gmail.com>
2026-05-04 14:03:34 +0200

    fix: style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import './Card.css' ;
+import PP from '../assets/PP.jpg';
 
 function Card(){
-
     return (
         <article className='card'>
-            <img src="./assets/PP.jpg" alt="profil picture" />
             <h2>JOJO</h2>
+            <img src={PP} alt="profil picture" />
             <h3>jojo@gmail.com</h3>
-            <p>Quel beau profil</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias ipsa porro, eum nulla aut saepe quas similique dolor molestiae exercitationem vero in praesentium. Officia odio quas omnis dolorem deserunt laudantium.</p>
         </article>
     )
 }
