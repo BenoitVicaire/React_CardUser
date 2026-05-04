faacc0c92372ecf365dfb553a4a5674d2e5e36d6
benoit <benoitvicaireformation@gmail.com>
2026-05-04 11:31:16 +0200

    Feat: User list / card

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import './Card.css' ;
+
+function Card(){
+
+    return (
+        <article className='card'>
+            <img src="./assets/PP.jpg" alt="profil picture" />
+            <h2>JOJO</h2>
+            <h3>jojo@gmail.com</h3>
+            <p>Quel beau profil</p>
+        </article>
+    )
+}
+
+export default Card;
