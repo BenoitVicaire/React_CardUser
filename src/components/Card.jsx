bc909993e0566fbea64c000d7180e8fbda0a5ea7
benoitvicaire <benoitvicaireformation@gmail.com>
2026-05-11 17:51:26 +0200

    exo fini

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,8 +2,23 @@ import './Card.css' ;
 import PP from '../assets/PP.jpg';
 
 function Card({image, pseudo, email, description}){
+    function handleMouseOver(event,classname){
+        event.currentTarget.classList.add(classname);
+    }
+    function handleMouseLeave(event,classname){
+        event.currentTarget.classList.remove(classname);
+    }
+
     return (
-        <article className='card'>
+        <article className='card'
+            onMouseOver={(event)=>{
+                event.stopPropagation();
+                handleMouseOver(event,"border");
+            }}
+            onMouseLeave={(event)=>{
+                event.stopPropagation();
+                handleMouseLeave(event,"border")}}
+        >
             <h2>{pseudo}</h2>
             <img src={image} alt="profil picture" />
             <h3>{email}</h3>
